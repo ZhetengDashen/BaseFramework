6d39393079f7bf505c182c8d2ef4629e23f24942
wangzq <sos181@163.com>
2019-08-26 16:04:38 +0800

    增加BaseDbBeanManager

---
+++ commonlibrary/src/main/java/com/baseeasy/commonlibrary/dao/daoutils/BaseDbBeanManager.java
@@ -0,0 +1,112 @@
+package com.baseeasy.commonlibrary.dao.daoutils;
+
+import org.greenrobot.greendao.AbstractDao;
+import org.greenrobot.greendao.query.QueryBuilder;
+
+import java.util.List;
+
+/**
+ * 作者：WangZhiQiang
+ * 时间：2019/8/26
+ * 邮箱：sos181@163.com
+ * 描述：封装常用方法封装
+ *
+ * 每创建一个Bean类就需要创建一个BeanManager 继承BaseBeanManager，用于管理数据库中的表
+ */
+public class BaseDbBeanManager<T, K> {
+    private AbstractDao<T, K> mDao;
+
+
+    public BaseDbBeanManager(AbstractDao dao) {
+        mDao = dao;
+    }
+
+
+    public  void save(T item) {
+        mDao.insert(item);
+    }
+
+    public void save(T... items) {
+        mDao.insertInTx(items);
+    }
+
+    public void save(List<T> items) {
+        mDao.insertInTx(items);
+    }
+
+    public void saveOrUpdate(T item) {
+        mDao.insertOrReplace(item);
+    }
+
+    public void saveOrUpdate(T... items) {
+        mDao.insertOrReplaceInTx(items);
+    }
+
+    public void saveOrUpdate(List<T> items) {
+        mDao.insertOrReplaceInTx(items);
+    }
+
+    public void deleteByKey(K key) {
+        mDao.deleteByKey(key);
+    }
+
+    public void delete(T item) {
+        mDao.delete(item);
+    }
+
+    public void delete(T... items) {
+        mDao.deleteInTx(items);
+    }
+
+    public void delete(List<T> items) {
+        mDao.deleteInTx(items);
+    }
+
+    public void deleteAll() {
+        mDao.deleteAll();
+    }
+
+
+    public void update(T item) {
+        mDao.update(item);
+    }
+
+    public void update(T... items) {
+        mDao.updateInTx(items);
+    }
+
+    public void update(List<T> items) {
+        mDao.updateInTx(items);
+    }
+
+    public T query(K key) {
+        return mDao.load(key);
+    }
+
+    public  List<T> queryAll() {
+        return mDao.loadAll();
+    }
+
+    public List<T> query(String where, String... params) {
+
+        return mDao.queryRaw(where, params);
+    }
+
+    public QueryBuilder<T> queryBuilder() {
+
+        return mDao.queryBuilder();
+    }
+
+    public long count() {
+        return mDao.count();
+    }
+
+    public void refresh(T item) {
+        mDao.refresh(item);
+
+    }
+
+    public void detach(T item) {
+        mDao.detach(item);
+    }
+}
