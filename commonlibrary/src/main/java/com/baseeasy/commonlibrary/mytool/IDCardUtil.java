deef378a3b41aac954a7c9446bca850cf21c8635
wangzq <sos181@163.com>
2020-01-15 14:48:25 +0800

    修改身份证号验证规则

--- commonlibrary/src/main/java/com/baseeasy/commonlibrary/mytool/IDCardUtil.java
+++ commonlibrary/src/main/java/com/baseeasy/commonlibrary/mytool/IDCardUtil.java
@@ -12,6 +12,8 @@ import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.regex.Pattern.*;
+
 /**
  * @Description：身份证号的util
  * @Author:
@@ -152,6 +154,8 @@ public class IDCardUtil {
      * @return 是否有效
      */
     public static boolean isValid(String id)throws ParseException{
+        id=chageIdCardEndWithX(id);
+
         Boolean validResult = true;
 //        //校验长度只能为15或18
 //        int len = id.length();
@@ -255,7 +259,7 @@ public class IDCardUtil {
      * @return
      */
     private static boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile("[0-9]*");
+        Pattern pattern = compile("[0-9]*");
         Matcher isNum = pattern.matcher(str);
         if (isNum.matches()) {
             return true;
@@ -270,8 +274,7 @@ public class IDCardUtil {
      * @return
      */
     public static boolean isDate(String strDate) {
-        Pattern pattern = Pattern
-                .compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
+        Pattern pattern = compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
         Matcher m = pattern.matcher(strDate);
         if (m.matches()) {
             return true;
@@ -346,6 +349,16 @@ public class IDCardUtil {
             return false;
         }
         return true;
+    }
+    //身份证号末尾为x时自动转大写
+    public static String chageIdCardEndWithX(String idcard){
+        if (idcard.length()>0){
+            if (idcard.charAt(idcard.length()-1)=='x'&& idcard.length()== 18) {
+                idcard=(new StringBuilder(idcard)).replace(idcard.length()-1,idcard.length(),"X").toString();
+            }
+        }
+        return idcard;
+
     }
     public static boolean isBlank(String str) {
         int strLen;
